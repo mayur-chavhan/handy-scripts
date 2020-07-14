754a22222b5254277ce81eecff59953f7402d5fd
Mayur Chavhan <b.chavhan.b@gmail.com>
2020-07-15 00:58:25 +0530

    Update prep_distro.sh

--- prep_distro.sh
+++ prep_distro.sh
@@ -13,6 +13,16 @@ echo 'Facebook: https://www.facebook.com/techwhale.in'
 echo ''
 
 
+upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
+secs=$((${upSeconds}%60))
+mins=$((${upSeconds}/60%60))
+hours=$((${upSeconds}/3600%24))
+days=$((${upSeconds}/86400))
+UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
+
+# get the load averages
+read one five fifteen rest < /proc/loadavg
+
 #
 # Prepare system for perfection devel
 #
