1f6829193de25cf20d77a93777b0c33877d0325c
Mayur Chavhan <b.chavhan.b@gmail.com>
2020-07-15 11:05:23 +0530

    Update prep_distro.sh

--- prep_distro.sh
+++ prep_distro.sh
@@ -13,19 +13,6 @@ echo 'Facebook: https://www.facebook.com/techwhale.in'
 echo ''
 
 
-upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
-secs=$((${upSeconds}%60))
-mins=$((${upSeconds}/60%60))
-hours=$((${upSeconds}/3600%24))
-days=$((${upSeconds}/86400))
-UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
-
-# get the load averages
-read one five fifteen rest < /proc/loadavg
-
-#
-# Prepare system for perfection devel
-#
 
 install_pip () {
         curl https://bootstrap.pypa.io/get-pip.py | $SUDO $PYTHON_BIN
