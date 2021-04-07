9ecd779ace504042b83a9f0d7d305feb9b235d97
Mayur Chavhan <b.chavhan.b@gmail.com>
2021-04-07 15:35:13 +0530

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,28 +1,35 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo true
+set -o errexit
+set -o nounset
 
-# Install kernel extra's to enable docker aufs support
-# sudo apt-get -y install linux-image-extra-$(uname -r)
+IFS=$(printf '\n\t')
 
-# Add Docker PPA and install latest version
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-# sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-# sudo apt-get update
+# Docker
+sudo apt remove --yes docker docker-engine docker.io containerd runc
+sudo apt update
+sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
+wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+sudo apt update
+sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
+sudo usermod --append --groups docker "$USER"
+sudo systemctl enable docker
+printf '\nDocker installed successfully\n\n'
 
-# Alternatively you can use the official docker install script
-wget -qO- https://get.docker.com/ | sh
+printf 'Waiting for Docker to start...\n\n'
+sleep 5
 
-# Install docker-compose
-COMPOSE_VERSION=$(git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1)
-
-sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
+# Docker Compose
+sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
 sudo chmod +x /usr/local/bin/docker-compose
+sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
 
-sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
-# Install docker-cleanup command
+printf '\nDocker Compose installed successfully\n\n'
+
+printf "Installing docker-cleanup command\n\n"
+
 cd /tmp || return
 
 git clone https://gist.github.com/76b450a0c986e576e98b.git
