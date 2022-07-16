47cc39145b489fd7c981dc3c2c4fa9d4cf1a8145
mayur.chavhan <b.chavhan.b@gmail.com>
2022-07-16 19:03:06 +0530

    🐛 BUG:

--- install-docker.sh
+++ install-docker.sh
@@ -24,7 +24,7 @@ else
   # Install docker-compose
   COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "([0-9]{1,4}(\.[0-9a-z]{1,6}){1,5})" | sort --version-sort | tail -n 1`
   echo -e "\nInstalling Docker-Compose Version: $COMPOSE_VERSION \n"
-  sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+  sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-linux-`uname -m` > /usr/local/bin/docker-compose"
   sudo chmod +x /usr/local/bin/docker-compose
   sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
   wait
