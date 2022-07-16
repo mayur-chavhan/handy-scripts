c1e057374b3fd2fa4090aa45c85663a20a63d4d6
mayur.chavhan <b.chavhan.b@gmail.com>
2022-07-16 17:57:55 +0530

    if condition

--- install-docker.sh
+++ install-docker.sh
@@ -5,24 +5,23 @@ sudo true
 #check docker is alreadu installed
 if [ -x "$(command -v docker)" ]; then
   echo 'Docker is already installed.'
-else
-  echo -e "\nInstalling Docker\n"
-  wget -qO- https://get.docker.com/ | sh
-  wait
-fi
+  elif
+    echo -e "\nInstalling Docker\n"
+    wget -qO- https://get.docker.com/ | sh
+    wait
 
-# check if docker-compose is already installed
-if [ -x "$(command -v docker-compose)" ]; then
-  echo 'Docker-compose is already installed.'
-else
-  echo -e "\nInstalling Docker-compose\n"
-  # Install docker-compose
-  COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "([0-9]{1,4}(\.[0-9a-z]{1,6}){1,5})" | sort --version-sort | tail -n 1`
-  echo -e "\nInstalling Docker-Compose Version: $COMPOSE_VERSION \n"
-  sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-  sudo chmod +x /usr/local/bin/docker-compose
-  sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
-  wait
+  # check if docker-compose is already installed
+  elif [ -x "$(command -v docker-compose)" ]; then
+    echo 'Docker-compose is already installed.'
+  else
+    echo -e "\nInstalling Docker-compose\n"
+    # Install docker-compose
+    COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "([0-9]{1,4}(\.[0-9a-z]{1,6}){1,5})" | sort --version-sort | tail -n 1`
+    echo -e "\nInstalling Docker-Compose Version: $COMPOSE_VERSION \n"
+    sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+    wait
 fi
 # Install docker-cleanup command
 echo -e "\nInstalling docker-cleanup tool\n"
